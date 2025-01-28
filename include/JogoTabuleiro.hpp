ff4ad9dc7868e70dd9ed47333bc04e8beeddcc36
Juliane <julianeguaracyruas@gmail.com>
2025-01-28 10:31:05 -0300

    Update JogoTabuleiro.hpp

--- include/JogoTabuleiro.hpp
+++ include/JogoTabuleiro.hpp
@@ -1,11 +1,11 @@
-#ifndef JOGOTABULEIRO_H
-#define JOGOTABULEIRO_H
+#ifndef JOGOTABULEIRO_HPP
+#define JOGOTABULEIRO_HPP
 
+#include <string>
 #include <vector>
 #include <stdexcept>
-#include <string>
 
-class JogoDeTabuleiro {
+class JogoDeTabuleiro{
 protected:
     std::vector<std::vector<char>> tabuleiro;
     int linhas, colunas;
@@ -13,20 +13,10 @@ protected:
 public:
     JogoDeTabuleiro(int l, int c);
 
-    // Inicializa o tabuleiro
     virtual void inicializar() = 0;
-
-    // Realiza uma jogada
     virtual bool fazerJogada(int jogador, int linha, int coluna) = 0;
-
-    // Verifica condição de vitória
     virtual bool verificarVitoria() = 0;
-
-    // Exibe o tabuleiro no console
     virtual void exibirTabuleiro() const;
-
-    // Virtual destructor
     virtual ~JogoDeTabuleiro() = default;
 };
-
 #endif

f0581a9ad2bad1afa07a1f757bf2c91473252f9f
Juliane <julianeguaracyruas@gmail.com>
2025-01-28 10:28:55 -0300

    Update JogoLig4.hpp

--- include/JogoLig4.hpp
+++ include/JogoLig4.hpp
@@ -1,26 +1,17 @@
-#ifndef JOGO_LIG4_HPP
-#define JOGO_LIG4_HPP
+#ifndef JOGOLIG4_HPP
+#define JOGOLIG4_HPP
 
 #include "JogoTabuleiro.hpp"
 #include <stdexcept>
 
-class JogoLig4 : public JogoDeTabuleiro {
+class JogoLig4 : public JogoDeTabuleiro{
 public:
     JogoLig4();
-
-    // Inicializa o tabuleiro com valores padrão
     void inicializar() override;
-
-    // Realiza uma jogada; pode lançar exceções em caso de erro
     bool fazerJogada(int jogador, int linha, int coluna) override;
-
-    // Verifica se há uma condição de vitória
     bool verificarVitoria() override;
-
-    // Exibe o estado atual do tabuleiro
     void exibirTabuleiro(); 
 
     ~JogoLig4() override = default;
 };
-
 #endif

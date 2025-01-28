63d5ebdfd42dc43c38d9da93bd836b0df46e10d9
Juliane <julianeguaracyruas@gmail.com>
2025-01-28 10:37:15 -0300

    Update JogoLig4.cpp

--- lib/JogoLig4.cpp
+++ lib/JogoLig4.cpp
@@ -1,25 +1,25 @@
 #include "JogoLig4.hpp"
 #include <iostream>
-#include <algorithm>
 #include <stdexcept>
+#include <algorithm>
 
 using namespace std;
 
-JogoLig4::JogoLig4() : JogoDeTabuleiro(6, 7) {}
+JogoLig4::JogoLig4() : JogoDeTabuleiro(6, 7){}
 
-void JogoLig4::inicializar() {
-    for (auto& linha : tabuleiro) {
+void JogoLig4::inicializar(){
+    for (auto& linha : tabuleiro){
         fill(linha.begin(), linha.end(), '-');
     }
 }
 
-bool JogoLig4::fazerJogada(int jogador, int linha, int coluna) {
-    if (coluna < 0 || coluna >= colunas) {
+bool JogoLig4::fazerJogada(int jogador, int linha, int coluna){
+    if (coluna < 0 || coluna >= colunas){
         throw out_of_range("ERRO: Coluna inválida!");
     }
 
-    for (int linha = linhas - 1; linha >= 0; --linha) {
-        if (tabuleiro[linha][coluna] == '-') {
+    for (int linha = linhas - 1; linha >= 0; --linha){
+        if (tabuleiro[linha][coluna] == '-'){
             tabuleiro[linha][coluna] = (jogador == 1) ? 'X' : 'O';
             return true;
         }
@@ -28,39 +28,36 @@ bool JogoLig4::fazerJogada(int jogador, int linha, int coluna) {
     throw invalid_argument("ERRO: Coluna cheia!");
 }
 
-bool JogoLig4::verificarVitoria() {
+bool JogoLig4::verificarVitoria(){
     const int CONSECUTIVO = 4;
-
-    // Verificar linhas
-    for (int linha = 0; linha < linhas; ++linha) {
-        for (int coluna = 0; coluna <= colunas - CONSECUTIVO; ++coluna) {
+//linhas
+    for (int linha = 0; linha < linhas; ++linha){
+        for (int coluna = 0; coluna <= colunas - CONSECUTIVO; ++coluna){
             char peca = tabuleiro[linha][coluna];
             if (peca != '-' && all_of(tabuleiro[linha].begin() + coluna, tabuleiro[linha].begin() + coluna + CONSECUTIVO,
-                                       [peca](char c) { return c == peca; })) {
+                                       [peca](char c) { return c == peca; })){
                 return true;
             }
         }
     }
-
-    // Verificar colunas
-    for (int coluna = 0; coluna < colunas; ++coluna) {
-        for (int linha = 0; linha <= linhas - CONSECUTIVO; ++linha) {
+//colunas
+    for (int coluna = 0; coluna < colunas; ++coluna){
+        for (int linha = 0; linha <= linhas - CONSECUTIVO; ++linha){
             char peca = tabuleiro[linha][coluna];
             if (peca != '-' && all_of(tabuleiro.begin() + linha, tabuleiro.begin() + linha + CONSECUTIVO,
-                                       [coluna, peca](const vector<char>& row) { return row[coluna] == peca; })) {
+                                       [coluna, peca](const vector<char>& row) { return row[coluna] == peca; })){
                 return true;
             }
         }
     }
-
-    // Verificar diagonais principais
-    for (int linha = 0; linha <= linhas - CONSECUTIVO; ++linha) {
-        for (int coluna = 0; coluna <= colunas - CONSECUTIVO; ++coluna) {
+//diagonais
+    for (int linha = 0; linha <= linhas - CONSECUTIVO; ++linha){
+        for (int coluna = 0; coluna <= colunas - CONSECUTIVO; ++coluna){
             char peca = tabuleiro[linha][coluna];
-            if (peca != '-') {
+            if (peca != '-'){
                 bool vitoria = true;
-                for (int i = 0; i < CONSECUTIVO; ++i) {
-                    if (tabuleiro[linha + i][coluna + i] != peca) {
+                for (int i = 0; i < CONSECUTIVO; ++i){
+                    if (tabuleiro[linha + i][coluna + i] != peca){
                         vitoria = false;
                         break;
                     }
@@ -70,14 +67,14 @@ bool JogoLig4::verificarVitoria() {
         }
     }
 
-    // Verificar diagonais secundárias
-    for (int linha = 0; linha <= linhas - CONSECUTIVO; ++linha) {
-        for (int coluna = CONSECUTIVO - 1; coluna < colunas; ++coluna) {
+    //diagonais secundárias
+    for (int linha = 0; linha <= linhas - CONSECUTIVO; ++linha){
+        for (int coluna = CONSECUTIVO - 1; coluna < colunas; ++coluna){
             char peca = tabuleiro[linha][coluna];
-            if (peca != '-') {
+            if (peca != '-'){
                 bool vitoria = true;
-                for (int i = 0; i < CONSECUTIVO; ++i) {
-                    if (tabuleiro[linha + i][coluna - i] != peca) {
+                for (int i = 0; i < CONSECUTIVO; ++i){
+                    if (tabuleiro[linha + i][coluna - i] != peca){
                         vitoria = false;
                         break;
                     }
@@ -90,10 +87,10 @@ bool JogoLig4::verificarVitoria() {
     return false;
 }
 
-void JogoLig4::exibirTabuleiro() {
+void JogoLig4::exibirTabuleiro(){
     cout << "Tabuleiro atual: " << endl;
-    for (int linha = 0; linha < linhas; ++linha) {
-        for (int coluna = 0; coluna < colunas; ++coluna) {
+    for (int linha = 0; linha < linhas; ++linha){
+        for (int coluna = 0; coluna < colunas; ++coluna){
             cout << tabuleiro[linha][coluna] << " ";
         }
         cout << endl;

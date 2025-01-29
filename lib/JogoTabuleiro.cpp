625e129a0d8895e888cde45d4d70c12f66c45914
Maria Eduarda Rodrigues Silva <mariaeduardarsva@gmail.com>
2025-01-28 21:57:19 -0300

    Update JogoTabuleiro.cpp
    Mostrar números das colunas e linhas para facilitar a visualização das jogadas.
    Adicionar o método jogadaValida: centraliza a validação de jogadas na classe base, reduzindo a repetição de código nos jogos individuais.

--- lib/JogoTabuleiro.cpp
+++ lib/JogoTabuleiro.cpp
@@ -3,18 +3,29 @@
 
 using namespace std;
 
-JogoDeTabuleiro::JogoDeTabuleiro(int l, int c) : linhas(l), colunas(c){
+JogoDeTabuleiro::JogoDeTabuleiro(int l, int c) : linhas(l), colunas(c) {
     if (l <= 0 || c <= 0) {
         throw invalid_argument("Dimensões do tabuleiro devem ser maiores que zero!");
     }
     tabuleiro = vector<vector<char>>(linhas, vector<char>(colunas, '-'));
 }
 
-void JogoDeTabuleiro::exibirTabuleiro() const{
-    for (const auto& linha : tabuleiro){
-        for (char celula : linha){
+void JogoDeTabuleiro::exibirTabuleiro() const {
+    cout << "  ";
+    for (int i = 0; i < colunas; i++) {
+        cout << i << " "; 
+    }
+    cout << endl;
+    
+    for (int i = 0; i < linhas; i++) {
+        cout << i << " ";
+        for (char celula : tabuleiro[i]) {
             cout << celula << " ";
         }
         cout << endl;
     }
+}
+
+bool JogoDeTabuleiro::jogadaValida(int linha, int coluna) const {
+    return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas && tabuleiro[linha][coluna] == '-';
 }
